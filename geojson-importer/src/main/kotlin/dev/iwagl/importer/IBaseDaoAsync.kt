ade11939122d6bf00f67f44d696d6991f60315a4
james.gliwa <james.gliwa@epicgames.com>
2023-03-13 22:46:38 -0700

    remove unused file

--- geojson-importer/src/main/kotlin/dev/iwagl/importer/IBaseDaoAsync.kt
+++
@@ -1,7 +0,0 @@
-package main.kotlin.dev.iwagl.importer
-
-interface IBaseDaoAsync<T : Any> {
-    suspend fun load(id: String) : T?
-    suspend fun save(obj : T) : Any?
-    suspend fun delete(id: String) : Boolean
-}
